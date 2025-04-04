92c66b0c1c7311b6e87c91d4e6ae76306754c521
Lixívia <38163500+LixiviaDev@users.noreply.github.com>
2025-04-04 19:13:50 +0200

    🔧 Trying to fix assets routing 🔧

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,8 +12,9 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-        {{-- <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script> --}}
+        {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+        <script type="module" src="{{ $_ENV["APP_URL"] . $_ENV["ASSETS_URL"] . $_ENV["ASSETS_JS"] }}"></script>
+        <link rel="stylesheet" href="{{ $_ENV["APP_URL"] . $_ENV["ASSETS_URL"] . $_ENV["ASSETS_CSS"] }}">
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">

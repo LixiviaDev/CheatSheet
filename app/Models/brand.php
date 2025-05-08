13e4248dcea1865b2ba381728b520b1d2f52ecac
Lixívia <38163500+LixiviaDev@users.noreply.github.com>
2025-05-09 01:57:58 +0200

    🔧 brand class changed to Brand 🔧

--- app/Models/brand.php
+++ app/Models/brand.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class brand extends Model
+class Brand extends Model
 {
     /** @use HasFactory<\Database\Factories\BrandFactory> */
     use HasFactory;

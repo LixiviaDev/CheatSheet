e537f44f6afdbe6031feed64ab7f1961acc1fa7a
Lixívia <38163500+LixiviaDev@users.noreply.github.com>
2025-05-09 02:30:29 +0200

    renaming

--- app/Models/offerItem.php
+++ app/Models/offerItem.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class offerItem extends Model
+class OfferItem extends Model
 {
     /** @use HasFactory<\Database\Factories\OfferItemFactory> */
     use HasFactory;
